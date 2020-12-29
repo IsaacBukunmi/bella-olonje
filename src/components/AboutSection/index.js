d06ca5a889b05f904994986a09047b34a9884c90
Isaac Adetona <adetonaisaac12345@gmail.com>
2020-12-29 13:51:50 +0100

    hero page animated

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -3,14 +3,36 @@ import styles from './index.module.scss';
 import register_phone from '../../assets/register-phone.png';
 import explore_phone from '../../assets/explore-phone.png';
 import checkout_phone from '../../assets/checkout-phone.png';
+import { motion } from 'framer-motion';
+
+const topHeaderVaraints = {
+    hidden:{
+        y:'-100vh'
+    },
+    visible:{
+        y:0,
+        
+        transition:{
+            type:'spring',
+            stiffness:120,
+            duration:2,
+            ease:'easeInOut'
+        }
+    }
+
+}
 
 const AboutSection = () => {
     return(
         <div className={styles._}>
             <div className={styles.container}>
-                <div className={styles.top_line_topic}>
+                <motion.div className={styles.top_line_topic}
+                    variants={topHeaderVaraints}
+                    initial="hidden"
+                    animate="visible"
+                >
                     <h2>How the app works</h2>
-                </div>
+                </motion.div>
                 <div className={styles.account_section}>
                     <div className={styles.account_image}>
                         <img src={register_phone} alt="register phone"/>
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,6 +1,96 @@
 import React from 'react';
 import styles from './index.module.scss';
-import mobile_showcase from '../../assets/two-mobile.png'
+import mobile_showcase from '../../assets/two-mobile.png';
+import { motion } from 'framer-motion'; 
+
+
+
+const pathVariants = {
+    hidden: {
+        opacity:0,
+        pathLenght:0
+    },
+    visible: {
+        opacity:1,
+        pathLenght:1,
+        transition:{
+            duration:2,
+            ease:"easeInOut"
+        }
+    }
+}
+
+const heroTextVaraints = {
+    hidden:{
+        opacity:0,
+        scale:.5
+    },
+    visible:{
+        opacity:1,
+        scale:1,
+        transition:{
+            ease:'easeOut',
+            duration:1
+        }
+    }
+}
+
+const smallTextVaraints = {
+    hidden:{
+        y:'-100vh'
+    },
+    visible:{
+        y:0,
+        
+        transition:{
+            type:'spring',
+            stiffness:30,
+            delay:2,
+            duration:.2,
+            ease:'easeInOut'
+        }
+    }
+
+}
+
+const buttonContainerVaraints = {
+    hidden:{
+        x:'-100vw'
+    },
+    visible:{
+        x:0,
+        
+        transition:{
+            type:'spring',
+            stiffness:120,
+            delay:1,
+        }
+    }
+
+}
+
+const appShowcaseVaraints = {
+    hidden:{
+        y:'10vh',
+        opacity:0,
+    },
+    visible:{
+        y:0,
+        opacity:1,
+        transition:{
+            type:'spring',
+            stiffness:120,
+            delay:3,
+            duration:0.5,
+             y: {
+                yoyo:Infinity,
+                duration:2,
+                ease:'easeInOut',
+             }
+        }
+    }
+}
+
 
 
 const HeroSection = () => {
@@ -9,25 +99,45 @@ const HeroSection = () => {
             <div className={styles._}>
                 <div className={styles.container}>
                     <div className={styles.content}>
-                        <h4>Food App</h4>
-                        <h1>Why stay hungry when you can order from Bella Olounje</h1>
-                        <p>Download the Bella Olounje Food App now on</p>
-                        <div className={styles.button_container}>
+                        <motion.h4
+                            variants={smallTextVaraints}
+                            initial="hidden"
+                            animate="visible"
+                        >Food App</motion.h4>
+                        <motion.h1
+                            variants={heroTextVaraints}
+                            initial="hidden"
+                            animate="visible"
+                        >Why stay hungry when you can order from Bella Olounje</motion.h1>
+                        <motion.p
+                            variants={buttonContainerVaraints}
+                            initial="hidden"
+                            animate="visible"
+                        >Download the Bella Olounje Food App now on</motion.p>
+                        <motion.div className={styles.button_container}
+                            variants={buttonContainerVaraints}
+                            initial="hidden"
+                            animate="visible"
+                        >
                             <button className={styles.playstore}>
                                 Playstore
                             </button>
                             <button className={styles.appstore}>
                                 App Store
                             </button>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
             </div>
-            <div className={styles.app_showcase}>
-                <div className={styles.app_images}>
+            <motion.div className={styles.app_showcase}
+                variants={appShowcaseVaraints}
+                initial="hidden"
+                animate="visible"
+            >
+                <motion.div className={styles.app_images}>
                     <img src={mobile_showcase} alt="mobile showcase"/>
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
         </>
     )
 }
